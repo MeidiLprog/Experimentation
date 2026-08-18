ff6c894c8ebf682da8a2e5944c6f78af7bb63388
Meidi Lefki <135418944+MeidiLprog@users.noreply.github.com>
2026-08-18 12:47:56 +0200

    conv between 2 doors resulting in a triangle

--- main.m
+++ main.m
@@ -1,33 +1,93 @@
 clc;
 close all;
 clear;
-%%References built
+
+%% References built
+
 T = tools();
 S = signals();
 
 Fe = 1000;
+dt = 1/Fe;
+
 f = 50;
 A = 10;
 
-Tmax = 1 /f;
+Tmax = 1/f;
 nbOSCI = 3;
 
-t = 0:1/Fe:Tmax * nbOSCI;
+%% Sinusoid part
+
+t = 0:dt:Tmax*nbOSCI;
 
 x = S.sinusoid(t,f,"sin");
+
 T.tempoGraph(t,x);
+
 [W,P] = T.EGraph(t,x);
 
 disp("Energy = " + W);
 disp("Average Power = " + P);
 
-%%door part:
-tdoor = 0:1/Fe:4;
+
+%% Door part
+
+tdoor = 0:dt:4;
+
 xD = S.door(tdoor,2,2,1);
+    
 T.tempoGraph(tdoor,xD);
 
 [Wdoor,Pdoor] = T.EGraph(tdoor,xD);
-disp("Door function E:" + Wdoor);
-disp("Door function power:" + Pdoor);
+
+disp("Door function E: " + Wdoor);
+disp("Door function power: " + Pdoor);
 
 
+%% Triangle part
+
+ttriangle = 0:dt:4;
+
+Atriangle = 2;
+t0triangle = 2;
+Ttriangle = 2;
+
+xTriangle = S.Triangle(ttriangle,Atriangle,t0triangle,Ttriangle);
+
+T.tempoGraph(ttriangle,xTriangle);
+
+[Wtriangle,Ptriangle] = T.EGraph(ttriangle,xTriangle);
+
+disp("Triangle function E: " + Wtriangle);
+disp("Triangle function power: " + Ptriangle);
+
+
+%% Convolution : Door * Door
+
+Tconv = 1;
+Aconv = 1;
+
+% We want each door to be defined on [0,Tconv]
+tDoorConv = 0:dt:Tconv;
+
+% Since Door() is centered around t0:
+% t0 - T/2 = 0
+% t0 + T/2 = T
+% therefore t0 = T/2
+t0conv = Tconv/2;
+
+x1 = S.door(tDoorConv,Aconv,t0conv,Tconv);
+x2 = S.door(tDoorConv,Aconv,t0conv,Tconv);
+
+% Display the two doors
+T.tempoGraph(tDoorConv,x1);
+T.tempoGraph(tDoorConv,x2);
+
+% Homemade convolution
+yconv = S.Convolution(x1,x2,dt);
+
+% Time axis of convolution
+tconv = 0:dt:(length(yconv)-1)*dt;
+
+% Display convolution result
+T.tempoGraph(tconv,yconv);
