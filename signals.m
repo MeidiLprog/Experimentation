c4867f9352aacc726a75c8e985e88526b6b9bec1
Meidi Lefki <135418944+MeidiLprog@users.noreply.github.com>
2026-08-18 12:42:08 +0200

    Adding discretized convolution calculation

--- signals.m
+++ signals.m
@@ -1,6 +1,8 @@
 function S = signals()
     S.sinusoid = @sinusoid;
     S.door = @Door;
+    S.Triangle = @Triangle;
+    S.Convolution = @Convolution;
 end
 
 
@@ -24,5 +26,40 @@ function x = Door(t,A,t0,T)
 
     x(index) = A; % Set the amplitude A for the duration of the door signal
       
+
+end
+
+
+function x = Triangle(t,A,t0,T)
+
+    x = zeros(size(t));
+    index = (t >= t0-T/2) & (t <= t0+T/2);
+    x(index) = A * (1 - 2*abs(t(index)-t0)/T);   
+end
+
+function y = Convolution(x,h,dt)
+
+    Nx = length(x);
+    Nh = length(h);
+
+    y = zeros(1,Nx + Nh - 1);
+
+    for n = 1:Nx + Nh - 1
+
+        for k = 1:Nx
+
+            j = n-k+1;
+
+            if j >= 1 && j <= Nh
+
+                y(n) = y(n) + x(k)*h(j);
+
+            end
+
+        end
+
+    end
+
+    y = y*dt;
 
 end
